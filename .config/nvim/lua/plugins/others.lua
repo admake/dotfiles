c8ad6f239a456b329c551e41ef5841ef10390328
Alexander Makeev <a.makeev@mts.ai>
2023-04-20 13:41:45 +0300

    [fix] touble telescope integration

--- .config/nvim/lua/plugins/others.lua
+++ .config/nvim/lua/plugins/others.lua
@@ -13,6 +13,18 @@ require("Comment").setup()
 
 require("nvim-web-devicons").setup()
 
+-- local actions = require("telescope.actions")
+local trouble = require("trouble.providers.telescope")
+
+require("telescope").setup({
+	defaults = {
+		mappings = {
+			i = { ["<c-t>"] = trouble.open_with_trouble },
+			n = { ["<c-t>"] = trouble.open_with_trouble },
+		},
+	},
+})
+
 require("trouble").setup({
 	-- your configuration comes here
 	-- or leave it empty to use the default settings

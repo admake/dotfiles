523d0b92c9bea0bddf7995f9151a154e50e7ba95
Alexander Makeev <ssh11ssh@gmail.com>
2026-06-26 14:36:03 +0300

    [misc] config hostty

--- .config/nvim/lua/plugins/autocolorscheme.lua
+++ .config/nvim/lua/plugins/autocolorscheme.lua
@@ -30,19 +30,6 @@ return {
     },
   },
   {
-    "sphamba/smear-cursor.nvim",
-    event = "VeryLazy",
-    opts = { -- Default  Range
-      stiffness = 0.6, -- [0, 1]
-      trailing_stiffness = 0.45, -- [0, 1]
-      stiffness_insert_mode = 0.5, -- 0.5 [0, 1]
-      trailing_stiffness_insert_mode = 0.5, -- 0.5 [0, 1]
-      damping = 0.85, -- 0.85 [0, 1]
-      damping_insert_mode = 0.9, -- 0.9 [0, 1]
-      distance_stop_animating = 0.1, -- 0.1 > 0
-    },
-  },
-  {
     "xzbdmw/colorful-menu.nvim",
     event = "VeryLazy",
     opts = {
